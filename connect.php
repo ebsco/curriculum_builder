8b5715c9d456846b3a6ca2e90da45186bf3c618b
Eric Frierson <efrierson@ebsco.com>
2018-05-09 11:11:06 -0500

    Stop tracking connect.php

--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$c = mysqli_connect('HOSTNAME', 'USERNAME', 'PASSWORD');
-	if (!$c) {
-		die('Could not connect: ' . mysqli_error($c));
-	}
-	$mysqldb = mysqli_select_db($c, 'DATABASENAME');
-?>
